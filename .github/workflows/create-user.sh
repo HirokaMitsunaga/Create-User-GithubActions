d223b8f7e82dff0bab6e970affffb5ef9168c420
HirokaMitsunaga <mitsunagahrk87@gmail.com>
2023-11-18 12:51:02 +0900

    use github secrets

--- .github/workflows/create-user.sh
+++ .github/workflows/create-user.sh
@@ -2,12 +2,12 @@
 
 # スキーマの作成（MySQLの例）
 echo "Creating schema..."
-mysql -u root -p$DB_PASSWORD -h $HOST_NAME -e "CREATE DATABASE IF NOT EXISTS $SCHEMA_NAME;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -h $HOST_NAME -e "CREATE DATABASE IF NOT EXISTS $SCHEMA_NAME;"
 
 # ユーザーの作成
 echo "Creating user..."
-mysql -u root -p$DB_PASSWORD -h $HOST_NAME -e "CREATE USER '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -h $HOST_NAME -e "CREATE USER '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASSWORD';"
 
 # 権限の付与
 echo "Granting privileges..."
-mysql -u root -p$DB_PASSWORD -h $HOST_NAME -e "GRANT ALL PRIVILEGES ON $SCHEMA_NAME.* TO '$USER_NAME'@'localhost';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -h $HOST_NAME -e "GRANT ALL PRIVILEGES ON $SCHEMA_NAME.* TO '$USER_NAME'@'%';"
